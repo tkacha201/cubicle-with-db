eb78210365293c49871290d50959f2bb0c4d3d84
Teodosi Kachakov <teodosi.kachakov@gmail.com>
2023-10-16 02:42:10 +0300

    CUB-P1: implement details page, provide error handling for wrong id

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -2,12 +2,10 @@ const router = require("express").Router();
 const cubeService = require("../services/cubeService");
 
 router.get("/create", (req, res) => {
-  console.log(cubeService.getAll());
   res.render("create");
 });
 
 router.post("/create", (req, res) => {
-  console.log(req.body);
   const { name, description, imageUrl, difficultyLevel } = req.body;
   cubeService.create({
     name,
@@ -16,6 +14,17 @@ router.post("/create", (req, res) => {
     difficultyLevel: Number(difficultyLevel),
   });
   res.redirect("/");
+});
+
+router.get("/:cubeId/details", (req, res) => {
+  const { cubeId } = req.params;
+  const cube = cubeService.getSingleCube(cubeId);
+
+  if (!cube) {
+    res.redirect("/404");
+    return;
+  }
+  res.render("details", { ...cube });
 });
 
 module.exports = router;
