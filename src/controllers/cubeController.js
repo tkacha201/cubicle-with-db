8aa2a7ee95483f973835f74f366761d75fbf6de5
Teodosi Kachakov <teodosi.kachakov@gmail.com>
2023-10-18 17:49:14 +0300

    get accessproes per cube without the owned ones

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -33,7 +33,14 @@ router.get("/:cubeId/attach-accessory", async (req, res) => {
   const { cubeId } = req.params;
   console.log(req.params.cubeId);
   const cube = await cubeService.getSingleCube(cubeId).lean();
-  const accessories = await accessoryService.getAll().lean();
+  const accessoryIds = cube.accessories
+    ? cube.accessories.map((a) => a._id)
+    : [];
+
+  const accessories = await accessoryService
+    .getWithoutOwned(accessoryIds)
+    .lean();
+
   const hasAccessories = accessories.length > 0; // view data, template data
 
   res.render("accessory/attach", { cube, accessories, hasAccessories });
--- src/services/accessoryService.js
+++ src/services/accessoryService.js
@@ -3,3 +3,9 @@ const Accessory = require("./../models/Accessory");
 exports.create = (accessoryData) => Accessory.create(accessoryData);
 
 exports.getAll = () => Accessory.find();
+
+exports.getWithoutOwned = (accessoryIds) => {
+  // $nin => NOT IN
+
+  return Accessory.find({ _id: { $nin: accessoryIds } });
+};
