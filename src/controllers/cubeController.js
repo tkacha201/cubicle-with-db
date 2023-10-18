600a6a86131cc2ed7c79172b41a3019c88f5644b
Teodosi Kachakov <teodosi.kachakov@gmail.com>
2023-10-18 11:57:00 +0300

    transfer all cube related view to separate folder

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const cubeService = require("../services/cubeService");
 
 router.get("/create", (req, res) => {
-  res.render("create");
+  res.render("cube/create");
 });
 
 router.post("/create", async (req, res) => {
@@ -24,7 +24,7 @@ router.get("/:cubeId/details", async (req, res) => {
     res.redirect("/404");
     return;
   }
-  res.render("details", { cube });
+  res.render("cube/details", { cube });
 });
 
 module.exports = router;
